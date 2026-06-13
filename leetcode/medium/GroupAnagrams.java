0515f580ac00e00abdac512602935b0db9b28645
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:51:00 +0530

    add Group Anagrams pointer

---
+++ leetcode/medium/GroupAnagrams.java
@@ -0,0 +1,8 @@
+// Group Anagrams (LeetCode #49 - Medium)
+// Solved in the strings folder (sorted word as hashmap key).
+// See: ../../strings/GroupAnagrams.java
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        System.out.println("see strings/GroupAnagrams.java");
+    }
+}
