c77528716dd9d5763dd08c5ceb1f5de2bd050786
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-15 13:02:25 +0530

    add Merge k Sorted Lists pointer (#160)

---
+++ leetcode/hard/MergeKSortedLists.java
@@ -0,0 +1,8 @@
+// Merge k Sorted Lists (LeetCode #23 - Hard)
+// Solved with a min-heap in the heap folder.
+// See: ../../heap/MergeKSortedLists.java
+public class MergeKSortedLists {
+    public static void main(String[] args) {
+        System.out.println("see heap/MergeKSortedLists.java");
+    }
+}
