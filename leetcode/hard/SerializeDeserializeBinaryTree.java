dc49698bd0ec0b522e509d9c832e83f8f5258438
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-15 13:02:39 +0530

    add Serialize Deserialize pointer

---
+++ leetcode/hard/SerializeDeserializeBinaryTree.java
@@ -0,0 +1,8 @@
+// Serialize and Deserialize Binary Tree (LeetCode #297 - Hard)
+// Solved with preorder + null markers in the trees folder.
+// See: ../../trees/SerializeDeserializeBinaryTree.java
+public class SerializeDeserializeBinaryTree {
+    public static void main(String[] args) {
+        System.out.println("see trees/SerializeDeserializeBinaryTree.java");
+    }
+}
